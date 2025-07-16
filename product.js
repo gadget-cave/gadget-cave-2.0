c4e813001b2eeb0ee30b2a3decd25736c8fdf556
gadget-cave <Laphixzam@gmail.com>
2025-07-17 04:43:51 +0530

    Update product.js

--- product.js
+++ product.js
@@ -1,23 +1,28 @@
 const products = {
-  "Earphones": [
-    { name: "Realme Buds 2", price: 499, image: "earbuds/realme_buds2.jpg" },
-    { name: "boAt BassHeads", price: 399, image: "earbuds/boat_bassheads.jpg" }
+  Earphones: [
+    {
+      name: "boAt BassHeads 100",
+      image: "https://m.media-amazon.com/images/I/71e9f2rMKfL._SL1500_.jpg",
+      price: "₹299"
+    },
+    {
+      name: "Realme Buds 2",
+      image: "https://m.media-amazon.com/images/I/71f11K0rJ5L._SL1500_.jpg",
+      price: "₹349"
+    }
   ],
-  "Watches": [
-    { name: "Titan Analog", price: 999, image: "watches/titan.jpg" },
-    { name: "Fastrack Digital", price: 1299, image: "watches/fastrack.jpg" }
+  Smartwatches: [
+    {
+      name: "boAt Wave Lite",
+      image: "https://m.media-amazon.com/images/I/61JLXfN3rKL._SL1500_.jpg",
+      price: "₹999"
+    }
   ],
-  "Smartwatches": [
-    { name: "Noise ColorFit", price: 1999, image: "smartwatches/noise_colorfit.jpg" },
-    { name: "boAt Storm", price: 1799, image: "smartwatches/boat_storm.jpg" }
-  ],
-  "Speakers": [
-    { name: "JBL Go", price: 1499, image: "speakers/jbl_go.jpg" },
-    { name: "boAt Stone", price: 1299, image: "speakers/boat_stone.jpg" }
-  ],
-  "Trimmers": [
-    { name: "Philips BT1232", price: 899, image: "trimmers/philips_bt1232.jpg" },
-    { name: "Mi Trimmer", price: 999, image: "trimmers/mi_trimmer.jpg" }
+  Trimmers: [
+    {
+      name: "MI Cordless Trimmer",
+      image: "https://m.media-amazon.com/images/I/61C4Zt3I3iL._SL1500_.jpg",
+      price: "₹699"
+    }
   ]
 };
-
