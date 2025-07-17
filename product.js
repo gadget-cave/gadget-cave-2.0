6211cd54b5d142e16358302de9169c85f4c2a9d2
gadget-cave <Laphixzam@gmail.com>
2025-07-18 01:52:23 +0530

    Update product.js

--- product.js
+++ product.js
@@ -1,16 +1,23 @@
-const productsData = {
-  "Earphones": [
-    { name: "boAt Bassheads 100", price: 499, image: "https://i.imgur.com/s9QOeKB.png" },
-    { name: "Realme Buds 2", price: 599, image: "https://i.imgur.com/qepSnrg.png" }
-  ],
-  "Smartwatches": [
-    { name: "Noise ColorFit Pulse", price: 1399, image: "https://i.imgur.com/EvddqHG.png" },
-    { name: "boAt Xtend", price: 1899, image: "https://i.imgur.com/C35jh8u.png" }
-  ],
-  "Speakers": [
-    { name: "boAt Stone 200", price: 999, image: "https://i.imgur.com/g6kqDL0.png" }
-  ],
-  "Power Banks": [
-    { name: "MI 10000mAh Power Bank", price: 799, image: "https://i.imgur.com/XFyRxFb.png" }
-  ]
-};
+const products = [
+  {
+    id: 1,
+    name: "Boat Earphones",
+    price: 499,
+    category: "Earphones",
+    image: "images/boat-earphone.jpg"
+  },
+  {
+    id: 2,
+    name: "Noise Smartwatch",
+    price: 1499,
+    category: "Watches",
+    image: "images/noise-watch.jpg"
+  },
+  {
+    id: 3,
+    name: "JBL Speaker",
+    price: 2499,
+    category: "Speakers",
+    image: "images/jbl-speaker.jpg"
+  }
+];
